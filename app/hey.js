9accfbce564a71d8721ab498c284b87674d9cdb9
dennismuturia <dmuturia2922@gmail.com>
2017-07-02 18:41:44 +0300

    Initial commit

---
+++ app/hey.js
@@ -0,0 +1,7 @@
+var findSum = function (first, second) {
+    var summ = first + second;
+    console.log(summ);
+};
+var first = parseInt(prompt("Enter the first number"));
+var second = parseInt(prompt("Enter the second number"));
+findSum(first, second);
---
+++ app/hey.ts
@@ -0,0 +1,9 @@
+var findSum = function(first:number, second:number){
+  var summ = first + second;
+  console.log(summ);
+}
+
+var first = parseInt(prompt("Enter the first number"));
+var second = parseInt(prompt("Enter the second number"));
+
+findSum(first, second);
